11605a70c58ed2e7d288ee5f476e0fedd74c7035
Kaja <111892274+kaja1998@users.noreply.github.com>
2023-07-23 17:13:40 +0200

    Sortierreihenfolge bei Customer integriert

--- src/shop/local/ui/gui/panels/E_ArticlesTablePanel.java
+++ src/shop/local/ui/gui/panels/E_ArticlesTablePanel.java
@@ -56,32 +56,3 @@ public class E_ArticlesTablePanel extends JTable {
 		sortByArticleTitle = true;
 	}
 }
-
-
-// Klasse ohne Sortierung
-
-//public class E_ArticlesTablePanel extends JTable {
-//
-//	public E_ArticlesTablePanel(java.util.List<Article> articles) {
-//		super();
-//
-//		// TableModel erzeugen ...
-//		E_ArticleTableModel tableModel = new E_ArticleTableModel(articles);
-//		// bei JTable "anmelden" und ...
-//		setModel(tableModel);
-//		// Daten an Model übergeben (für Sortierung o.ä.)
-//		updateArticlesList(articles);
-//	}
-//
-//	public void updateArticlesList(java.util.List<Article> articles) {
-//
-//		// Sortierung (mit Lambda-Expression)
-////		Collections.sort(articles, (a1, a2) -> a1.getTitel().compareTo(a2.getTitel()));	// Sortierung nach Titel
-//		Collections.sort(articles, (a1, a2) -> a1.getNumber() - a2.getNumber());	// Sortierung nach Nummer
-//
-//		// TableModel von JTable holen und ...
-//		E_ArticleTableModel tableModel = (E_ArticleTableModel) getModel();
-//		// Inhalt aktualisieren
-//		tableModel.setArticles(articles);
-//	}
-//}
